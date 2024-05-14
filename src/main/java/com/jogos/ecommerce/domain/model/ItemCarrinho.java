e01e96a05dcf5c3606ee42cf8f9158c5ce47e555
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-05-14 08:36:49 -0300

    FEAT: modelo ItemCarrinho Base

---
+++ src/main/java/com/jogos/ecommerce/domain/model/ItemCarrinho.java
@@ -0,0 +1,40 @@
+package com.jogos.ecommerce.domain.model;
+
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+public class ItemCarrinho {
+    
+      
+    @Id
+    // indetity -> forma nativa do sgbd(no caso auto increment.Para casos em que o banco é criado manualmente. AUTO é para quando for criando junto com a API)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name="cod_item_carrinho")
+    private Long id;
+    
+    @Column(name="cod_carrinho")
+    private Carrinho carrinho;
+
+    @Column(name="cod_produto")
+    private Produto produto;
+
+    private Long quantidade;
+    private float preco_unitario;
+}
